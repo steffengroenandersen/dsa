29b82b1708a5be7e5dcd58fc2076b1d5ad28e94d
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-05 15:25:33 +0100

    Add exercises for binary search

---
+++ 02-dsa-binary-search/02-binary-search-comparator.js
@@ -0,0 +1,2 @@
+// Binary Search (compare function)
+// https://petlatkea.notion.site/Binary-Search-compare-function-601fa270ac40450ea51c925a9a9a7f4e
---
+++ 02-dsa-binary-search/02-binary-search-recursive.js
@@ -0,0 +1,2 @@
+// Binary Search (recursive)
+//https://petlatkea.notion.site/Binary-Search-recursive-ef8d8c591cbe4802b881d9f5e3c586fd
