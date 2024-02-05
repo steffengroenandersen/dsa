e542a4fb6271443900483c819c532e8dd93ac7bc
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-05 14:43:52 +0100

    Add binary search

--- 02-dsa-binary-search/script.js
+++ 02-dsa-binary-search/script.js
@@ -1,3 +1,44 @@
-const values = [1, 2, 3, 5, 8, 9, 11, 12, 14, 15];
+function binarySearch(value, values) {
+  console.log("Starting binarySearch...");
 
-console.log(values);
+  let start = 0;
+  let end = values.length - 1;
+  let iteration = 1;
+
+  while (start <= end) {
+    console.log();
+    console.log("Iteration: " + iteration);
+    iteration++;
+
+    let middle = Math.floor((start + end) / 2);
+
+    console.log("Start: " + start);
+    console.log("End: " + end);
+    console.log("Middle: " + middle);
+    console.log("Values[middle] == " + values[middle]);
+
+    // If correct
+    if (value === values[middle]) {
+      console.log("Index is: " + middle);
+      return values[middle];
+    }
+
+    // If too low
+    if (value < values[middle]) {
+      console.log(value + " is lower than " + values[middle]);
+      end = middle - 1;
+    }
+
+    // If too high
+    else {
+      console.log(value + " is higher than " + values[middle]);
+      start = middle + 1;
+    }
+  }
+
+  console.log("Value not found in the array.");
+}
+
+const values = [1, 2, 3, 4, 5, 8, 10, 12, 15, 20];
+const targetValue = 9;
+binarySearch(targetValue, values);
