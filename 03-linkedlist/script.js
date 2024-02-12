fc7f54fd917e3832413366461133886c4a9e7837
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-12 22:56:37 +0100

    Add methods addFirst and addLast

--- 03-linkedlist/script.js
+++ 03-linkedlist/script.js
@@ -4,22 +4,91 @@ window.addEventListener("load", start);
 
 function start() {
   console.log("STARTING PROGRAM...");
-  const hello = createLinkedList();
 
-  console.log(hello);
-}
+  const head = {
+    next: null,
+    data: "HEAD",
+  };
 
-function createLinkedList() {
-  return new LinkedList("hello", "world");
+  const tail = {
+    prev: null,
+    data: "TAIL",
+  };
+
+  const node1 = {
+    prev: null,
+    next: null,
+    data: "A",
+  };
+
+  const node2 = {
+    prev: null,
+    next: null,
+    data: "B",
+  };
+
+  const node3 = {
+    prev: null,
+    next: null,
+    data: "C",
+  };
+
+  const newList = new LinkedList(head, tail);
+  newList.addLast(node1);
+  newList.addLast(node2);
+  newList.addLast(node3);
+  newList.dumpList();
+
+  const node4 = {
+    data: "D",
+  };
+  //newList.addLast(node4);
+  newList.addFirst(node4);
+  newList.dumpList();
+
+  //const emptyList = new LinkedList();
+  //emptyList.dumpList();
 }
 
 class LinkedList {
-  constructor(param1, param2) {
-    this.param1 = param1;
-    this.param2 = param2;
+  constructor() {
+    this.tail = { prev: null, data: "TAIL" };
+    this.head = { next: this.tail, data: "HEAD" };
+    this.tail.prev = this.head;
   }
 
-  method1() {
-    console.log(param1 + param2);
+  dumpList() {
+    console.log("dumpList()");
+
+    let aNode = this.head;
+
+    while (aNode != null) {
+      console.log(`
+        node: ${aNode.data}
+        ------------
+        prev: ${aNode.prev ? aNode.prev.data : null}
+        next: ${aNode.next ? aNode.next.data : null}
+      `);
+
+      aNode = aNode.next;
+    }
+  }
+
+  addLast(newNode) {
+    console.log("addLast()");
+
+    newNode.prev = this.tail.prev;
+    newNode.prev.next = newNode;
+    newNode.next = this.tail;
+    this.tail.prev = newNode;
+  }
+
+  addFirst(newNode) {
+    console.log("addFirst()");
+
+    newNode.prev = this.head;
+    newNode.next = this.head.next;
+    newNode.next.prev = newNode;
+    this.head.next = newNode;
   }
 }
