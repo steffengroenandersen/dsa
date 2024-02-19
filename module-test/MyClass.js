292a167a3845a0c316c5b66854580d447cb7fe51
steffengroenandersen <steffengroenandersen@gmail.com>
2024-02-19 14:27:50 +0100

    Add module exercise

---
+++ module-test/MyClass.js
@@ -0,0 +1,9 @@
+export default class MyClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  toString() {
+    return "MyClass with name " + this.name;
+  }
+}
---
+++ module-test/script.js
@@ -0,0 +1,6 @@
+import MyClass from "./MyClass.js"; // Import from other files
+
+console.log("PROGRAM START...");
+
+const instans = new MyClass("Steffen");
+console.log("Instansen hedder " + instans);
